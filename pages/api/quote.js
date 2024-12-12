bf198bf30e31360484ade92679f1ae8969f60022
Klima-Nrw <129694970+Klima-Nrw@users.noreply.github.com>
2024-12-13 01:38:46 +0500

    Update quote.js -- mail enabled

--- pages/api/quote.js
+++ pages/api/quote.js
@@ -44,7 +44,7 @@ export default async function handler(req, res) {
 
     try {
       await transporter.sendMail(mailOptions);
-     // await transporter.sendMail(mailOptions2);
+      await transporter.sendMail(mailOptions2);
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (error) {
       console.error(error);
