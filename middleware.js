7e6b5fd7130c2bd3d2be67fcad7a4f2752963eb3
Klima-Nrw <129694970+Klima-Nrw@users.noreply.github.com>
2025-01-01 05:52:43 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -5,38 +5,9 @@ const allowedCountries = ['DE', 'PK', 'USA']; // Country codes for Germany (DE),
 
 export async function middleware(req) {
   // Get the user's IP address from headers
-  const ip = req.headers.get('x-forwarded-for') || req.ip;
+ console.log("middleware working");
 
-  // If the IP is from localhost, skip geolocation check
-  if (ip === '::1' || ip === 'localhost:3000') {
-    console.log('Running locally, skipping country check.');
-  //  return NextResponse.next(); // Allow the request to continue
-  }
-
-  console.log(`User IP: ${ip}`);
-
- // try {
-    // Fetch user location using ipinfo.io
-  //  const response = await axios.get(`https://ipinfo.io/${ip}/json`);
-   // const userCountry = response.data.country;
-
-    console.log(`User Country: ${userCountry}`);
-
-    // Check if the user's country is allowed
-    //if (allowedCountries.includes(userCountry)) {
-     // return NextResponse.next(); // Allow access
-   // } else {
-      // If country is not allowed, return a 403 response
-    //  return new NextResponse('Access denied.\n This site is only available in Germany.', {
-     //   status: 403,
-     // });
-   // }
- // } catch (error) {
-  //  console.error('Error fetching geolocation data:', error);
-   // return new NextResponse('Error determining your location.', { status: 500 });
-//  }
 }
-
 // Middleware configuration to apply to all routes
 export const config = {
   matcher: ['/'],
