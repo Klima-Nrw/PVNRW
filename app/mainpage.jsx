44724f59c0bf9c70b347b1656e20d5b656fdd3e6
Klima-Nrw <129694970+Klima-Nrw@users.noreply.github.com>
2025-01-30 13:00:05 +0100

    Update mainpage.jsx

--- app/mainpage.jsx
+++ app/mainpage.jsx
@@ -95,10 +95,82 @@ export default function Page() {
               />
             </div>
           </div>
-          {/* Weitere Inhalte */}
+          <div className="row mt-5">
+            <div className="col-md-3 col-sm-6 mb-3 mb-md-0">
+              <div className="mb-3">
+                <svg
+                  className="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={48}
+                  height={48}
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <h4>Energiekosten sparen</h4>
+              <p>Bis zu 80% weniger Stromkosten mit unseren Photovoltaikanlagen!</p>
+            </div>
+            <div className="col-md-3 col-sm-6 mb-3 mb-md-0">
+              <div className="mb-3">
+                <svg
+                  className="bi bi-house-fill"
+                  fill="currentColor"
+                  height={48}
+                  viewBox="0 0 16 16"
+                  width={48}
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6a.5.5 0 0 0 .708.708L8 2.207l5.646 5.647a.5.5 0 0 0 .708-.708l-6-6z" />
+                  <path d="M13 10v4a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1z" />
+                </svg>
+              </div>
+              <h4>Experteninstallation</h4>
+              <p>Über 10 Jahre Erfahrung in Solarenergie und Photovoltaik-Installation!</p>
+            </div>
+            <div className="col-md-3 col-sm-6 mb-3 mb-md-0">
+              <div className="mb-3">
+                <svg
+                  className="bi bi-check-circle-fill"
+                  fill="currentColor"
+                  height={48}
+                  viewBox="0 0 16 16"
+                  width={48}
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.93 10.768a.75.75 0 0 0 1.06-.02l3.482-4.216a.75.75 0 1 0-1.137-.978l-3.02 3.66-1.47-1.353a.75.75 0 0 0-1.037 1.086l2 1.844z" />
+                </svg>
+              </div>
+              <h4>Kundenzufriedenheit</h4>
+              <p>Werden Sie Teil unserer zufriedenen Solarhaushalte!</p>
+            </div>
+            <div className="col-md-3 col-sm-6">
+              <div className="mb-3">
+                <svg
+                  className="w-6 h-6 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={48}
+                  height={48}
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12.356 3.066a1 1 0 0 0-.712 0l-7 2.666A1 1 0 0 0 4 6.68a17.695 17.695 0 0 0 2.022 7.98 17.405 17.405 0 0 0 5.403 6.158 1 1 0 0 0 1.15 0 17.406 17.406 0 0 0 5.402-6.157A17.694 17.694 0 0 0 20 6.68a1 1 0 0 0-.644-.949l-7-2.666Z" />
+                </svg>
+              </div>
+              <h4>Umweltfreundlich</h4>
+              <p>Nachhaltige Energie für eine grünere Zukunft!</p>
+            </div>
+          </div>
         </div>
       </section>
-      {/* Weitere Abschnitte */}
+      {/* Weitere Inhalte */}
     </>
   );
 }
