cecc8e0ca8a155b7e5d0bfde7b98f0cf60ec7e66
Klima-Nrw <129694970+Klima-Nrw@users.noreply.github.com>
2025-02-01 22:32:49 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,9 +5,9 @@ import Link from 'next/link';
 
 import Headers from "../components/header"
 import Footer from "../components/footer"
- 
+
 const NoSSR = dynamic(() => import('./mainpage'), { ssr: true })
- 
+
 export default function Page() {
   const [isClient, setIsClient] = useState(false);
 
@@ -17,18 +17,40 @@ export default function Page() {
 
 
   return isClient ? (
-    
+
     <>
 
+    <script
+             type="application/ld+json"
+             dangerouslySetInnerHTML={{
+               __html: JSON.stringify({
+                 "@context": "https://schema.org/",
+                 "@type": "Product",
+                 "name": "Photovoltaik Montage Dorsten",
+                 "image": "https://www.pvnrw.de/img/about.png",
+                 "description": "Professionelle Montage von Photovoltaik-Anlagen für private und gewerbliche Kunden. Nachhaltige und effiziente Installation für maximale Energieerträge.",
+                 "sku": "PV-MONTAGE-001",
+                 "brand": {
+                   "@type": "Brand",
+                   "name": "PVNRW"
+                 },
+                 "aggregateRating": {
+                   "@type": "AggregateRating",
+                   "ratingValue": "4.8",
+                   "reviewCount": "227"
+                 }
+               })
+             }}
+           />
 <Headers />
-  
+
 
     <div>
       <NoSSR />
     </div>
 
    <Footer />
-   
+
     </>
   ): null;
 }
