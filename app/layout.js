9d926fc65bc60621ebab564c103e23126860913c
Klima-Nrw <129694970+Klima-Nrw@users.noreply.github.com>
2025-03-24 03:05:30 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -91,22 +91,7 @@ export default function RootLayout({ children }) {
 
 
 
-<Script
-        id="tawk-to-script"
-        strategy="afterInteractive" // Ensures the script loads after the page is interactive
-      >
-        {`
-          var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-          (function() {
-            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/6761c83f49e2fd8dfef98b46/default';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-          })();
-        `}
-      </Script>
+
 
 
       </body>
