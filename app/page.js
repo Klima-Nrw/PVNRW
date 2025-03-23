3f9375119ebd157e7756a07e66f02acb71118d04
Klima-Nrw <129694970+Klima-Nrw@users.noreply.github.com>
2025-03-24 03:24:46 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -43,6 +43,15 @@ export default function Page() {
              }}
            />
 
+             <meta property="og:title" content="PVNrw | Wir installieren Solaranlagen" />
+        <meta
+          property="og:description"
+          content="PV NRW – Ihr Experte für Photovoltaik-Lösungen in Nordrhein-Westfalen. Wir bieten maßgeschneiderte Solaranlagen für Privat- und Gewerbekunden, um eine nachhaltige Energiezukunft zu schaffen."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://pvnrw.de" />
+            <meta property="og:image" content="https://www.pvnrw.de/img/logo.png" />
+
        
 <Headers />
 
