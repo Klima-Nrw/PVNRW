e1485b073fcb8d14b6665e11f5ca519fb422b87a
Arturfreelancer <admanagerartur@gmail.com>
2025-04-18 11:00:52 +0100

    Update contact.js

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -44,3 +44,10 @@ export default async function handler(req, res) {
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WMVZRX7N');</script>
+<!-- End Google Tag Manager -->
